287b473e97aa90ad62d666a4832bc3dd16fbcd4e
ull4775-creator <ull4775@gmail.com>
2026-06-24 15:05:31 -0700

    Fix middleware references and clear cache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,13 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Security Headers Middleware
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
         
-        // HAPUS atau COMMENT baris BlockSuspiciousIP ini:
-        // $middleware->append(\App\Http\Middleware\BlockSuspiciousIP::class);
-        
-        // Alias untuk middleware groups
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
