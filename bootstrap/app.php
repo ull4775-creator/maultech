279689afc7787a7b0352dab19b8ace633d3fc919
ull4775-creator <ull4775@gmail.com>
2026-06-24 14:54:21 -0700

    Remove BlockSuspiciousIP middleware reference

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
-        $middleware->append(\App\Http\Middleware\BlockSuspiciousIP::class);
+        // $middleware->append(\App\Http\Middleware\BlockSuspiciousIP::class);
         
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
