5a8eb377e3e1a9e1c691409c2f0411afa8311c15
ull4775-creator <ull4775@gmail.com>
2026-06-24 15:13:30 -0700

    Add BlockSuspiciousIP middleware

--- app/Http/Middleware/BlockSuspiciousIP.php
+++ app/Http/Middleware/BlockSuspiciousIP.php
@@ -4,19 +4,11 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class BlockSuspiciousIP
 {
     public function handle(Request $request, Closure $next)
     {
-        $ip = $request->ip();
-        
-        // Check if IP is blocked
-        if(Cache::has('blocked_ip_' . $ip)) {
-            abort(403, 'Your IP has been blocked due to suspicious activity.');
-        }
-        
         return $next($request);
     }
 }
